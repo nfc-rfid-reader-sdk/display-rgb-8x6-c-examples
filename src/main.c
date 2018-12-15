c2bee1bb3ff1d399e33e5a7f324b7dcf7411e151
developer3 <support@d-logic.rs>
2018-12-15 14:20:36 +0100

    Split code to: set_default_message() and set_display_configuration()

--- src/main.c
+++ src/main.c
@@ -20,12 +20,19 @@
 #	define Sleep(ms)		usleep((ms)*1000)
 #endif
 
+/*** ---------------------------------------------------------- ***/
+
 #define MIN_LIB_VER		"2.1.0"
 
-DL_STATUS status;
+#define NUMBER_OF_PANELS			12
+#define NUMBER_OF_ROWS				4
 
 /* zero is broadcast - only one LED Display can be connected to the bus */
 int display_id = 0;
+
+/*** ---------------------------------------------------------- ***/
+
+DL_STATUS status;
 
 void prn_status(char * chPreText)
 {
@@ -33,10 +40,61 @@ void prn_status(char * chPreText)
 	fflush(stdout);
 }
 
+/*** ---------------------------------------------------------- ***/
+
+void set_display_configuration(void)
+{
+	int number_of_panels = NUMBER_OF_PANELS;
+	int number_of_rows = NUMBER_OF_ROWS;
+	int font = 2;
+	int brightness = 10;
+	int scroll_start_ms = 1000;
+	int scroll_speed_ms = 300;
+	int screen_display_time = 5000; //
+	int red = 182;
+	int green = 255;
+	int blue = 0;
+
+	status = DL_DisplaySendConfigRGB(display_id, number_of_panels,
+			number_of_rows, font, brightness, scroll_start_ms, scroll_speed_ms,
+			screen_display_time, red, green, blue);
+
+	prn_status("DL_DisplaySendConfigRGB");
+}
+
+/*** ---------------------------------------------------------- ***/
+
+/** Set default display message, available on startup */
+void set_default_message(void)
+{
+	int number_of_panels = NUMBER_OF_PANELS;
+	int number_of_rows = NUMBER_OF_ROWS;
+	int font = 2;
+	int brightness = 5;
+	int scroll_start_ms = 1000;
+	int scroll_speed_ms = 500;
+	int screen_display_time = 5000; //
+	int red = 0;
+	int green = 255;
+	int blue = 255;
+
+	char *default_msg = "DEFAULT MESSAGE";
+	int default_msg_len = strlen(default_msg);
+
+	printf("Test write default message in the %d\n", display_id); /* prints Test UART on ARM */
+
+	status = DL_DisplaySetDefaultRgb(display_id, number_of_panels,
+			number_of_rows, font, brightness, scroll_start_ms, scroll_speed_ms,
+			screen_display_time, default_msg, default_msg_len, red, green,
+			blue);
+
+	prn_status("DL_DisplaySetDefaultRgb");
+}
+
+/*** ---------------------------------------------------------- ***/
+
 int main(void)
 {
-	int i;
-
 	puts("Test LED display library API functions.");
 
 	printf("Minimal library version is: ");
@@ -106,7 +164,7 @@ int main(void)
 		printf("Current LED Display time is= (timestamp= %u) %s\n", timestamp,
 				ctime((const time_t *) &timestamp));
 
-	// ctime() convert timestamp to (local time) string
+		// ctime() convert timestamp to (local time) string
 	}
 
 	/*--- Set the display time and check ---*/
@@ -137,33 +195,21 @@ int main(void)
 
 	/* ---------------------------------------------------------- */
 
+	set_default_message();
+
+	/* ---------------------------------------------------------- */
+
 	/*
 	 * Both configuration and sending text is in one function:
-	 * DL_DisplaySendConfigRGB() + DL_DisplaySendText() = DL_DisplayCommonRgb()
+	 * DL_DisplaySendConfigRGB() + DL_DisplaySendText() => DL_DisplayCommonRgb()
 	 *
 	 * example use separated functions.
 	 */
 
 	/*--- configuration ---*/
+	set_display_configuration();
 
-	int number_of_panels = 12;
-	int number_of_rows = 4;
-	int font = 2;
-	int brightness = 10;
-	int scroll_start_ms = 1000;
-	int scroll_speed_ms = 300;
-	int screen_display_time = 5000; //
-	int red = 50;
-	int green = 100;
-	int blue = 150;
-
-	status = DL_DisplaySendConfigRGB(display_id, number_of_panels,
-			number_of_rows, font, brightness, scroll_start_ms, scroll_speed_ms,
-			screen_display_time, red, green, blue);
-
-	prn_status("DL_DisplaySendConfigRGB");
-
-	/*--- send text, multiple times... ---*/
+	/*--- send text ---*/
 
 	char *text_content = "SAMPLE TEXT LINE";
 	int text_len = strlen(text_content);
@@ -174,24 +220,10 @@ int main(void)
 
 	/* ---------------------------------------------------------- */
 
-	/*--- Set default message, store in display, available after restart ---*/
-
-	char *default_msg = "DEFAULT MESSAGE";
-	int default_msg_len = strlen(default_msg);
-
-	printf("Test write default message in the %d\n", display_id); /* prints Test UART on ARM */
-
-	status = DL_DisplaySetDefaultRgb(display_id, number_of_panels,
-			number_of_rows, font, brightness, scroll_start_ms, scroll_speed_ms,
-			screen_display_time, default_msg, default_msg_len, red, green,
-			blue);
-
-	/* ---------------------------------------------------------- */
-
 	puts("Wait for 10 seconds, then the display will reset !");
 	fflush(stdout);
 
-	for (i = 0; i < 10; ++i)
+	for (int i = 0; i < 10; ++i)
 	{
 		printf(".");
 		fflush(stdout);
@@ -199,6 +231,8 @@ int main(void)
 		Sleep(1000);
 	}
 	printf("\n");
+
+	/* ---------------------------------------------------------- */
 
 	status = DL_DisplayReset(display_id);
 
